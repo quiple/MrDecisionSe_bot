2453796efddc8c3e354b722eeddd74d47ba5c2ea
Minseo Lee <itoupluk427@gmail.com>
2024-01-17 22:36:32 +0900

    Update discordbot.js

--- discordbot.js
+++ discordbot.js
@@ -1,6 +1,11 @@
 const { Client, GatewayIntentBits } = require("discord.js");
 const client = new Client({
-  intents: [GatewayIntentBits.MessageContent, GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.DirectMessages],
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.MessageContent,
+  ],
 });
 
 const config = require("./config.js");
@@ -12,7 +17,8 @@ client.on("ready", () => {
 
 const mrDecisionBot = require("./mrDecisionBot.js");
 client.on("messageCreate", (msg) => {
-  /* discord wrapper */ let reply = mrDecisionBot.discord(msg.content);
+  /* discord wrapper */
+  let reply = mrDecisionBot.discord(msg.content);
   if (reply !== null) {
     console.log(reply);
     msg.reply(reply.a);
