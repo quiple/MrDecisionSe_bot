86e4e90f539f5f5172ce988f3528f0015f818176
Minseo Lee <itoupluk427@gmail.com>
2024-01-17 19:41:41 +0900

    Modify

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-const { Telegraf } = require('telegraf')
-// config.sample.js 파일을 참조해주십시요.
-const config = require('./config.js');
-const bot = new Telegraf(config.token)
+const { Telegraf } = require("telegraf");
+// config.sample.js 파일을 참조해 주세요.
+const config = require("./config.js");
+const bot = new Telegraf(config.token);
 
-const mrDecisionBot = require('./mrDecisionBot.js');
+const mrDecisionBot = require("./mrDecisionBot.js");
 
-bot.start((ctx) => ctx.reply(mrDecisionBot.helpMessage))
-bot.help((ctx) => ctx.reply(mrDecisionBot.helpMessage))
-bot.command('/about', (ctx) => ctx.reply(mrDecisionBot.aboutText))
-bot.catch((err) => { console.error('Telegram Error : ', err) })
-bot.on('text', (ctx) => {
-  let response = mrDecisionBot.process(ctx);  
-  ctx.reply(response.a)
-})
+bot.start((ctx) => ctx.reply(mrDecisionBot.helpMessage));
+bot.help((ctx) => ctx.reply(mrDecisionBot.helpMessage));
+bot.command("/about", (ctx) => ctx.reply(mrDecisionBot.aboutText));
+bot.catch((err) => {
+  console.error("Telegram Error: ", err);
+});
+bot.on("text", (ctx) => {
+  let response = mrDecisionBot.process(ctx);
+  ctx.reply(response.a);
+});
 
-bot.startPolling()
+bot.startPolling();
--- discordbot.js
+++ discordbot.js
@@ -1,19 +1,19 @@
-const Discord = require('discord.js');
-const config = require('./config.js');
+const Discord = require("discord.js");
+const config = require("./config.js");
 const client = new Discord.Client();
 
-client.on('ready', () => {
+client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-const mrDecisionBot = require('./mrDecisionBot.js');
+const mrDecisionBot = require("./mrDecisionBot.js");
 
-client.on('message', msg => {
+client.on("message", (msg) => {
   /* discord wrapper */
-  let reply = mrDecisionBot.discord(msg.content)
-  if(reply !== null) {
+  let reply = mrDecisionBot.discord(msg.content);
+  if (reply !== null) {
     console.log(reply);
-    msg.reply(reply.a);  
+    msg.reply(reply.a);
   }
 });
 
