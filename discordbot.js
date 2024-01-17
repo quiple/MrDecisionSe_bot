ffb5d5bfe7e5ee69fda98b590848bd52f3d66f3c
Minseo Lee <itoupluk427@gmail.com>
2024-01-17 22:32:31 +0900

    Update Discord.js

--- discordbot.js
+++ discordbot.js
@@ -1,20 +1,20 @@
-const Discord = require("discord.js");
+const { Client, GatewayIntentBits } = require("discord.js");
+const client = new Client({
+  intents: [GatewayIntentBits.MessageContent, GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.DirectMessages],
+});
+
 const config = require("./config.js");
-const client = new Discord.Client();
+client.login(config.discord_token);
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 const mrDecisionBot = require("./mrDecisionBot.js");
-
-client.on("message", (msg) => {
-  /* discord wrapper */
-  let reply = mrDecisionBot.discord(msg.content);
+client.on("messageCreate", (msg) => {
+  /* discord wrapper */ let reply = mrDecisionBot.discord(msg.content);
   if (reply !== null) {
     console.log(reply);
     msg.reply(reply.a);
   }
 });
-
-client.login(config.discord_token);
