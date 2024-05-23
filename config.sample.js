5cb61e92b1363c315fdc1c8d6141c4f99d98b1ba
Minseo Lee <itoupluk427@gmail.com>
2024-05-23 14:26:16 +0900

    Update config.sample.js

--- config.sample.js
+++ config.sample.js
@@ -1,13 +1,10 @@
-module.exports = {
-  token: 'your telegram token is here',
-  // Line Message API
-  CHANNEL_ACCESS_TOKEN: 'line channel access token is here',
-  CHANNEL_SECRET: 'line channel secret code is here',
-  discord_token: 'discord token is here',
-  SLACK_SIGNING_SECRET: 'slack signing secret is here',
-  SLACK_BOT_TOKEN: 'slack bot token is here',
-  bluesky_handle: 'bluesky bot handle is here',
-  bluesky_app_password: 'bluesky app password is here',
-  bluesky_owner: 'bluesky owner handle is here',
-  bluesky_owner_contact: 'bluesky owner contact is here',
-}
+export const token = 'your telegram token is here',
+  CHANNEL_ACCESS_TOKEN = 'line channel access token is here',
+  CHANNEL_SECRET = 'line channel secret code is here',
+  discord_token = 'discord token is here',
+  SLACK_SIGNING_SECRET = 'slack signing secret is here',
+  SLACK_BOT_TOKEN = 'slack bot token is here',
+  bluesky_handle = 'bluesky bot handle is here',
+  bluesky_app_password = 'bluesky app password is here',
+  bluesky_owner = 'bluesky owner handle is here',
+  bluesky_owner_contact = 'bluesky owner contact is here'
