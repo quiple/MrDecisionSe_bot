6912d28113f952e02598b79afaf34e0c89a6bed8
Minseo Lee <itoupluk427@gmail.com>
2024-05-29 10:28:43 +0900

    이(가) 할까

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
 import { mrDecisionBot } from './mrDecisionBot.js'
 
-console.log(mrDecisionBot.discord('먹을까'))
+console.log(mrDecisionBot.discord('이길까'))
+console.log(mrDecisionBot.discord('한화가 이길까'))
