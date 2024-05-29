6d6c031d1d94ac90b4915bcb7801c2d72a043c78
Minseo Lee <itoupluk427@gmail.com>
2024-05-29 16:33:14 +0900

    갈길이멀다

--- test.js
+++ test.js
@@ -2,3 +2,6 @@ import { mrDecisionBot } from './mrDecisionBot.js'
 
 console.log(mrDecisionBot.discord('이길까'))
 console.log(mrDecisionBot.discord('한화가 이길까'))
+console.log(mrDecisionBot.discord('한화가 이겼을까'))
+console.log(mrDecisionBot.discord('한화가 이겨야할까'))
+console.log(mrDecisionBot.discord('한화가 이겼어야할까'))
